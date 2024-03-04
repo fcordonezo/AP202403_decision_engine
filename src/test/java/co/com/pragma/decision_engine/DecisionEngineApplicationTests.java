97be88126fca1fb21b580b8e42f26dadd337fcea
Fabian Camilo Ordoñez <camilo.ordonezo@pragma.com.co>
2024-03-03 19:26:07 -0500

    feat!: Se inicia proyecto de motor de decisiones

---
+++ src/test/java/co/com/pragma/decision_engine/DecisionEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.pragma.decision_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecisionEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
