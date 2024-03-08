699f082746b9cfdae5b83d243f129c603cebcf89
Fabian Camilo Ordoñez <camilo.ordonezo@pragma.com.co>
2024-03-07 21:46:59 -0500

    docs: Se agrega funcionalidad para swagger

--- src/main/java/co/com/pragma/decision_engine/DecisionEngineApplication.java
+++ src/main/java/co/com/pragma/decision_engine/DecisionEngineApplication.java
@@ -1,8 +1,11 @@
 package co.com.pragma.decision_engine;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info=@Info(title="Motor de decisiones"))
 @SpringBootApplication
 public class DecisionEngineApplication {
 
--- src/main/java/co/com/pragma/decision_engine/infrastructure/in/rest/DecisionEngineController.java
+++ src/main/java/co/com/pragma/decision_engine/infrastructure/in/rest/DecisionEngineController.java
@@ -3,6 +3,7 @@ package co.com.pragma.decision_engine.infrastructure.in.rest;
 import co.com.pragma.decision_engine.application.dto.DecisionEngineRequestDto;
 import co.com.pragma.decision_engine.application.dto.DecisionEngineResponseDto;
 import co.com.pragma.decision_engine.application.handler.DecisionEngineHandler;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "decision-engine")
 public class DecisionEngineController {
   private final DecisionEngineHandler decisionEngineHandler;
 
@@ -20,6 +20,7 @@ public class DecisionEngineController {
     this.decisionEngineHandler = decisionEngineHandler;
   }
 
+  @Operation(summary = "Ejecutar motor de decisiones")
   @PostMapping(path = "/run", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<DecisionEngineResponseDto> run(@RequestBody DecisionEngineRequestDto dto) {
     DecisionEngineResponseDto decisionEngineResponseDto = decisionEngineHandler.run(dto);
