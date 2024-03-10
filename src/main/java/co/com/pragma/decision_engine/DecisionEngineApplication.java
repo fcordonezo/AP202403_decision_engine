4ef393ea67ff86b08b3991bbce979ea4d3f0bd86
Fabian Camilo Ordoñez <camilo.ordonezo@pragma.com.co>
2024-03-09 22:50:07 -0500

    feat: Se agrega implementación JWT

--- src/main/java/co/com/pragma/decision_engine/DecisionEngineApplication.java
+++ src/main/java/co/com/pragma/decision_engine/DecisionEngineApplication.java
@@ -1,11 +1,14 @@
 package co.com.pragma.decision_engine;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(info=@Info(title="Motor de decisiones"))
+@OpenAPIDefinition(info=@Info(title="Motor de decisiones", version = "1.0.0"))
+@SecurityScheme(name = "BearerAuthentication", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer")
 @SpringBootApplication
 public class DecisionEngineApplication {
 
--- src/main/java/co/com/pragma/decision_engine/infrastructure/in/rest/DecisionEngineController.java
+++ src/main/java/co/com/pragma/decision_engine/infrastructure/in/rest/DecisionEngineController.java
@@ -4,14 +4,14 @@ import co.com.pragma.decision_engine.application.dto.DecisionEngineRequestDto;
 import co.com.pragma.decision_engine.application.dto.DecisionEngineResponseDto;
 import co.com.pragma.decision_engine.application.handler.DecisionEngineHandler;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
+@SecurityRequirement(name = "BearerAuthentication")
 public class DecisionEngineController {
   private final DecisionEngineHandler decisionEngineHandler;
 
